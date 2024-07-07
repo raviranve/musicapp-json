89f45581ffbaacce535b98b093d53d09787301ad
Ravi Ranve <you@example.com>
2024-07-07 12:53:34 +0530

    feat: json-server

---
+++ api/server.js
@@ -0,0 +1,22 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+    console.log(`Json server is running on port ${PORT}`)
+})
+
+// Export the Server API
+module.exports = server
